2935442a68816ba71fb2c8450743bdf8edf5d091
Reginaldo Motta <reginaldomcv@gmail.com>
2021-03-08 19:31:04 -0400

    Account registration

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+    
+    return response.status(201).send();
+});
+
+app.listen(3333);
