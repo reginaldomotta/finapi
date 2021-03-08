7b11a4d7baa1706e7ec4a13fdef2c37c45154ef5
Reginaldo Motta <reginaldomcv@gmail.com>
2021-03-08 19:32:42 -0400

    CPF Validation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
     
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists){
+    return response.status(400).json({error: "Customer already exists!"});        
+    }
+    
     customers.push({
         cpf,
         name,
