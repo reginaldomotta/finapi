d1cb1437e0db61b73d19b40191902415f5a9e852
Reginaldo Motta <reginaldomcv@gmail.com>
2021-03-08 21:02:05 -0400

    deposit created

--- src/index.js
+++ src/index.js
@@ -47,4 +47,20 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
 app.listen(3333);
